31f9d4fe12097ed1dc75b870b38f49b4fdecd80a
burmjeff <burmjeff@gmail.com>
2024-01-02 20:13:48 -0500

    fix pl groups

--- backend/app/models/playlist_group_model.go
+++ backend/app/models/playlist_group_model.go
@@ -2,6 +2,6 @@ package models
 
 // Channel struct to describe Playlist Group object.
 type PlaylistGroup struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name" validate:"required,lte=255"`
+	ID   int64  `db:"id" json:"id"`
+	Name string `db:"name" json:"name" validate:"required,lte=255"`
 }
